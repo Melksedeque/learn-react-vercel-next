dda3a75b2b204334bca69f49877274559ba05e6e
Melksedeque <freelancer@melksedeque.com.br>
2025-07-04 12:10:50 -0300

    Conversão de index.html em index.jsx

---
+++ index.jsx
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+
+function Header({title}) {
+    return <h1>{title? title : "Default title"}</h1>;
+}
+
+function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+    const [likes, setLikes] = useState(0);
+
+    function handleClick() {
+        console.log('I like it!');
+        setLikes(likes + 1);
+    }
+
+    return (
+        <>
+            <Header title="Develop. Preview. Ship." />
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+            <button onClick={handleClick}>Likes ({likes})</button>
+        </>  
+    )
+}
